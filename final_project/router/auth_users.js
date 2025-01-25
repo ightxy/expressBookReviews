36ae0f74340ac6ea7ab3a684fca0867db81ddae6
ightxy <dmcgrew723@gmail.com>
2025-01-25 16:20:24 -0500

    added review functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -42,7 +42,41 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const review = req.body.review;
+  const isbn = req.body.isbn;
+  const book = Object.values(books).find(book => book.isbn === isbn);
+
+  if (!book) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  if(!book.reviews) {
+    book.reviews={};
+  } 
+
+  book.reviews[username] = review;
+  
+  return res.status(200).json({message: `Review for ${book.title} by ${username} has been successfully added/modified.`,
+  reviews: book.reviews});
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const username = req.body.username;
+    const isbn = req.params.isbn;
+    const book = Object.values(books).find(book => book.isbn === isbn);
+
+    if (!book) {
+        return res.status(404).json({message: 'Book not found'});
+    }
+
+    if(book.reviews && book.reviews[username]) {
+        delete book.reviews[username];
+        return res.status(200).json({message: `Review for ${book.title} by ${username} has been successfully deleted`,
+     reviews: book.reviews});
+    } else {
+        return res.status(400).json({message: `No review found for ${book.title} by ${username}.`});
+    }
 });
 
 module.exports.authenticated = regd_users;
