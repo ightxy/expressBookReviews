73ee31fb052fa7099f9e9bb28e34f6d6cf0161a4
ightxy <dmcgrew723@gmail.com>
2025-01-25 15:30:37 -0500

    updated general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,25 +20,32 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.send(JSON.stringify(books[isbn]));
+  const isbnList = Object.values(books).map(book => book.isbn);
+  return res.send(isbnList);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const authorList = Object.values(books).map(book => book.author);
+  return res.send(authorList);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here\
+  const title = req.params.title;
+  const titleList = Object.values(books).map(book => book.title);
+  return res.send(titleList);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const reviews = req.params.reviews;
+  const reviewList = Object.values(books).map(book => book.review);
+  return res.send(reviewList);
 });
 
 module.exports.general = public_users;
